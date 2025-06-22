e6b2dbf9c47003671eafd32762d98bfc96943730
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2025-06-22 20:18:45 -0300

    implementar testes para controller de usuarios. Testes criados -> telaIndex, telaLogin, telaCadastro, cadastrarUsuario

--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,52 @@
 package br.com.gerenciamento.controller;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.gerenciamento.model.Usuario;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioControllerTest {
+
+    @Autowired
+    private UsuarioController usuarioController;
+
+    @Test
+    public void telaIndex() {
+        ModelAndView modelAndView = usuarioController.index();
+        Assert.assertEquals("home/index", modelAndView.getViewName());
+        Assert.assertNotNull(modelAndView.getModel().get("aluno"));
+    }
+
+    @Test
+    public void telaLogin() {
+        ModelAndView modelAndView = usuarioController.login();
+        Assert.assertEquals("login/login", modelAndView.getViewName());
+        Assert.assertNotNull(modelAndView.getModel().get("usuario"));
+    }
+
+    @Test
+    public void telaCadastro() {
+        ModelAndView modelAndView = usuarioController.cadastrar();
+        Assert.assertEquals("login/cadastro", modelAndView.getViewName());
+        Assert.assertNotNull(modelAndView.getModel().get("usuario"));
+    }
+
+
+    @Test
+    public void cadastrarUsuario() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("teste@email.com");
+        usuario.setUser("testelogin");
+        usuario.setSenha("123456");
+
+        ModelAndView modelAndView = usuarioController.cadastrar(usuario);
+        Assert.assertEquals("redirect:/", modelAndView.getViewName());
+    }
 }
